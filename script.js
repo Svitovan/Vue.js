8471b6a9190ca9603dd5a3e8446ccb06e452573d
Your Name <AYN@euro-center.com>
2020-09-15 16:51:25 +0200

    lesson 8

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
+Vue.component('product', {
+    template: `
+    <h1>Component #1</h1>
+    `
+})
+
 let app = new Vue ({
     el: "#app",
     data: {
-        product: 'Socks (new)',
-        image: './img/green-socks.jpg',
+        brand: 'Vue Mastery',
+        product: 'Socks',
+        selectedVariant: 0,
         link: 'http://anekdot.ru',
-        inventory: 7,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
               variantId: 2234,
               variantColor: 'Green',
-              variantImage: './img/green-socks.jpg'
+              variantImage: './img/green-socks.jpg',
+              variantQuantity: 111
+
             },
             {
               variantId: 2235,
               variantColor: 'Blue',
-              variantImage: './img/blue-socks.jpg'
+              variantImage: './img/blue-socks.jpg',
+              variantQuantity: 0
+
             }
           ],
-        cart: 0
+        cart: 0,
+        onSale: false
     },
-    methods:{
+    methods: {
         addToCart: function(){
             this.cart += 1
         },
         delFromCart: function() {
             this.cart -= 1
         },
-        updateProduct: function(variantImage){
-            this.image = variantImage
+        updateProduct: function(index){
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return 'Sale!'
+            }
+            return '...'
         }
     }
 })
