b9387948a91f1714800b35d789e064358592a9fe
Alex <svitovan@outlook.com>
2020-09-16 22:16:50 +0200

    Component

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
     template: `
-    <h1>Component #1</h1>
-    `
-})
-
-let app = new Vue ({
-    el: "#app",
-    data: {
+    <div class="product">
+        <div class="product-image">
+            <img v-bind:src="image" alt="Socks">
+        </div>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p>{{ sale }}</p>
+            <p v-if="inStock">In stock</p>
+            <p v-else>Out of stock</p>
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+            <div v-for="(variant, index) in variants"
+                v-bind:key="variant.variantId"
+                class="color-box"
+                v-bind:style="{ backgroundColor: variant.variantColor}"
+                v-on:mouseover="updateProduct(index)">
+            </div>
+            <!-- <a v-bind:href="link">More products like this</a> -->
+            <button v-on:click="addToCart"
+                    v-bind:disabled="!inStock"
+                    v-bind:class="{disabledButton: !inStock}">Add to Cart</button><br>
+            <button v-on:click="delFromCart">Delete from Cart</button>
+            <div class="cart">
+                <p>Cart({{cart}})</p>
+            </div>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
         brand: 'Vue Mastery',
         product: 'Socks',
         selectedVariant: 0,
@@ -30,6 +60,7 @@ let app = new Vue ({
           ],
         cart: 0,
         onSale: false
+        }
     },
     methods: {
         addToCart: function(){
@@ -59,5 +90,13 @@ let app = new Vue ({
             return '...'
         }
     }
+})
+
+let app = new Vue ({
+    el: "#app",
+    data: {
+        premium: true
+    }
+
 })
 
